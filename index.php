8903a739c3b7226b7dd89e7a651ffdcb5792ef12
Charles Severance <csev@umich.edu>
2019-05-10 12:51:59 -0400

    Moar UI

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
-<h1>Hello Virginia World</h1>
+<?php
+require_once("top.php");
+?>
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<?php
+$OUTPUT->bodyStart();
+?>
+<div id="container">
+ <a name="index">
+ <div id="page1" class="jumbotron">
+    <a name="index"></a>
+<h1>Welcome to 
+<?php
+if ( strlen($CFG->servicename) > 0 ) {
+    echo(htmlentities($CFG->servicename));
+} else {
+    echo('Test Tsugi Server');
+}
+echo("</h1>\n");
+
+echo('<p><a href="tsugi">Go to the Tsugi Dashboard</a>.</p>'."\n");
+
+if ( strlen($CFG->ownername) > 0 ) {
+    echo("<p>Please contact ". $CFG->ownername." for questions about this server</p>\n");
+}
+?>
+<p>For questions about TsugiCloud services, please contact
+info at learnxp.com.
+</p>
+
+</div>
+</div>
+
+<?php
+$OUTPUT->footerStart();
+?>
+<script>
+$('body').prepend('<img src="images/bgbg.jpg" onload="this.style.opacity=\'1\';" \
+  style=" z-index: -1000; position: fixed; top:-10%; height: 110%; width: 100%; \
+   opacity:0; -moz-transition: opacity 2s; -webkit-transition: opacity 2s; \
+  -o-transition: opacity 2s; transition: opacity 2s; \
+">');
+</script>
+<?php
+$OUTPUT->footerEnd();
+?>
